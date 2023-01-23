a1f5d08e10a8dbe68c34d3eb77c5a31d86ea0002
Narayan Dubey <narayan.dubey@smarterp.com>
2023-01-24 00:37:51 +0530

    added projects

--- src/components/ui/button/Button.js
+++ src/components/ui/button/Button.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { motion } from "framer-motion"
 import "./style.css"
 
-const Button = ({ children, startIcon = "", endIcon = "", title = "",onClick=()=>{} }) => {
+const Button = ({ children, startIcon = "", endIcon = "", title = "",onClick=()=>{},style={} }) => {
   return (
     <motion.button
       whileHover={{ scale: 1.5 }}
       whileTap={{ scale: 0.95 }}
       className="styled-button"
       onClick={()=>onClick()}
+      style={style}
     >
       <span className='space-between'>
         {startIcon}
--- src/pages/home/component/projects/Project.js
+++ src/pages/home/component/projects/Project.js
@@ -1,19 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+import { Button, LfBox } from "../../../../components";
+import "./style.css";
 
 const Project = () => {
+  const [selectedId, setSelectedId] = useState(null);
+  const items = [
+    { id: 1, title: "Pragya RO System", subtitle: "A small e-commerce site for a shop.",tech:["React","Express","Material Ui"] },
+    { id: 2, title: "Pragya RO System App", subtitle: "A small e-commerce app for a shop.",tech:["React-Native","Express"] },
+  ];
   return (
-    <div id='Project' className='page-container'>
-      <div className='width100'>
+    <div id="Project" className="page-container">
+      <div className="width100">
         <h1>Project</h1>
       </div>
-      <div className='left'>left</div>
-      <div className='right'>
-        <h2>Project Name</h2>
-        <p>Description</p>
+      <div className="projects-container">
+        {items.map((item) => (
+          <motion.div
+            className="small card pointer"
+            layoutId={item.id}
+            onClick={() => setSelectedId(item)}
+            key={item.id}
+            whileHover={{ scale: 1.1 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+          >
+            <motion.img
+              className="thumbnail"
+              src="https://www.projectmanager.com/wp-content/uploads/2021/10/211014_Blog_Feature_Project_Environment-scaled.jpg"
+            />
+            <motion.h2>{item.title}</motion.h2>
+            <motion.h5>{item.subtitle}</motion.h5>
+          </motion.div>
+        ))}
+
+        <AnimatePresence>
+          {selectedId && (
+            <motion.div className="detail card space-between" layoutId={selectedId?.id}>
+               <motion.button
+                  className="close-btn"
+                  onClick={() => setSelectedId(null)}
+                >
+                  X
+                </motion.button>
+              <div className="detail-left">
+                <motion.img
+                  className="thumbnail"
+                  src="https://www.projectmanager.com/wp-content/uploads/2021/10/211014_Blog_Feature_Project_Environment-scaled.jpg"
+                />
+              </div>
+              <div className="detail-right">
+               
+                <motion.h3>{selectedId?.title}</motion.h3>
+                <motion.h5>{selectedId?.subtitle}</motion.h5>
+                <motion.div>
+                  {selectedId?.tech.map((elem,index)=>(<motion.p key={index}>{index+1}. {elem}</motion.p>))}      
+                </motion.div>
+                <motion.div>
+                  <Button>Demo</Button>
+                  <Button style={{marginLeft:"10px"}}>Code</Button>
+                </motion.div>
+              </div>
+              {/* <motion.h5>{item.subtitle}</motion.h5>
+       <motion.h2>{item.title}</motion.h2> */}
+            </motion.div>
+          )}
+        </AnimatePresence>
+        {/* 
+<motion.div
+      layout
+      data-isOpen={isOpen}
+      initial={{ borderRadius: 50 }}
+      className="parent"
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <motion.div layout className="child" />
+    </motion.div> */}
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
